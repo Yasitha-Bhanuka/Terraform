ce2103385bef2588e6f96d63c58727c6bf4db8a8
Yasitha-Dev <yasithabhanukac@gmail.com>
2026-03-01 17:22:40 +0530

    added the docker registry for add docker image into GC

--- terraform/service.tf
+++ terraform/service.tf
@@ -1,3 +1,11 @@
+resource "google_artifact_registry_repository" "yasitha_docker_registry_repository" {
+  location = "asia-southeast1"
+
+  repository_id = "yasitha-docker-registry-repository"
+  format        = "DOCKER"
+}
+
+
 resource "docker_registry_image" "yasitha_registry_image" {
   name          = docker_image.yasitha-demo-docker-image.name
   keep_remotely = true
